d9a0f54cd46fa8d467524d9ea5ff550ce646af0b
Jakub Kopka <kuba@surfportal.pl>
2017-11-02 09:45:47 +0100

    - Kosmetyczne poprawki (między innymi: Poprawienie wyśweotlania nazw w PA)

--- stacjapogodowa/models.py
+++ stacjapogodowa/models.py
@@ -8,6 +8,10 @@ class Odczyty(models.Model):
     sila_wiatru = models.FloatField()
     kierunek_wiatru = models.CharField(max_length=3)
 
+    class Meta:
+        verbose_name = ("Odczyt")
+        verbose_name_plural = ("Odczyty")
+
 
 class Odczyty_Algorytm(models.Model):
     data_odczytu = models.DateTimeField()
@@ -16,3 +20,8 @@ class Odczyty_Algorytm(models.Model):
     cisnienie = models.FloatField()
     sila_wiatru = models.FloatField()
     kierunek_wiatru = models.CharField(max_length=3)
+
+    class Meta:
+        verbose_name = ("Odczyt stworozny przez algorytm")
+        verbose_name_plural = ("Odczyty stworzone przez algorytm")
+
